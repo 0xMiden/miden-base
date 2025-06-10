5452188e510cbada3d5836a912664bb35835188d
Marti <marti@miden.team>
2025-06-10 12:58:50 +0000

    test different scenarios for burn and distribute

--- crates/miden-testing/tests/integration/scripts/faucet.rs
+++ crates/miden-testing/tests/integration/scripts/faucet.rs
@@ -1,8 +1,10 @@
 extern crate alloc;
 
+use miden_crypto::Word;
 use miden_lib::{
     errors::tx_kernel_errors::ERR_FUNGIBLE_ASSET_DISTRIBUTE_WOULD_CAUSE_MAX_SUPPLY_TO_BE_EXCEEDED,
-    transaction::TransactionKernel,
+    note::utils::build_p2id_recipient,
+    transaction::{TransactionKernel, TransactionKernelError},
 };
 use miden_objects::{
     Felt,
@@ -11,7 +13,8 @@ use miden_objects::{
     transaction::{OutputNote, TransactionScript},
 };
 use miden_testing::{Auth, MockChain, MockFungibleFaucet};
-use miden_tx::utils::word_to_masm_push_string;
+use miden_tx::{TransactionExecutorError, utils::word_to_masm_push_string};
+use vm_processor::ExecutionError;
 
 use crate::{
     assert_transaction_executor_error, get_note_with_fungible_asset_and_script,
@@ -213,4 +216,125 @@ fn prove_faucet_contract_burn_fungible_asset_succeeds() {
     // check that the account burned the asset
     assert_eq!(executed_transaction.account_delta().nonce(), Some(Felt::new(3)));
     assert_eq!(executed_transaction.input_notes().get_note(0).id(), note.id());
+}
+
+#[test]
+fn burn_and_mint_fungible_asset() {
+    let mut mock_chain = MockChain::new();
+    let faucet = mock_chain.add_pending_existing_faucet(Auth::BasicAuth, "TST", u64::MAX, Some(1));
+    let faucet_other =
+        mock_chain.add_pending_existing_faucet(Auth::BasicAuth, "TST", u64::MAX, Some(1));
+
+    let mut faucet_account = faucet.account().clone();
+    let receiver = mock_chain.add_pending_existing_wallet(Auth::BasicAuth, Vec::new());
+
+    let fungible_asset = FungibleAsset::new(faucet.account().id(), 1).unwrap();
+
+    let recipient = build_p2id_recipient(receiver.id(), Word::default()).unwrap();
+
+    let aux = Felt::new(27);
+    let tag = NoteTag::for_local_use_case(123, 0).unwrap();
+    let amount = Felt::new(250);
+    let note_execution_hint = NoteExecutionHint::Always;
+    let note_type = NoteType::Private;
+
+    tag.validate(note_type).expect("note tag should support private notes");
+
+    let note_script = format!(
+        "
+        # burn the asset
+        begin
+            dropw
+
+            # pad the stack before call
+            padw padw padw padw
+            # => [pad(16)]
+
+            exec.::miden::note::get_assets drop
+            mem_loadw
+            # => [ASSET, pad(12)]
+
+            call.::miden::contracts::faucets::basic_fungible::burn
+            dropw dropw dropw dropw
+
+            push.{recipient}
+            push.{note_execution_hint}
+            push.{note_type}
+            push.{aux}
+            push.{tag}
+            push.{amount}
+            # => [amount, tag, aux, note_type, execution_hint, RECIPIENT, pad(7)]
+
+            call.::miden::contracts::faucets::basic_fungible::distribute
+            # => [note_idx, pad(15)]
+
+            # truncate the stack
+            dropw dropw dropw dropw
+        end",
+        note_type = note_type as u8,
+        recipient = word_to_masm_push_string(&recipient.digest()),
+        note_execution_hint = Felt::from(note_execution_hint)
+    );
+
+    let note = get_note_with_fungible_asset_and_script(fungible_asset, note_script.as_str());
+
+    mock_chain.add_pending_note(OutputNote::Full(note.clone()));
+    mock_chain.prove_next_block();
+
+    // Execute the transaction against the other faucet. Should fail.
+    let executed_transaction_other = mock_chain
+        .build_tx_context(faucet_other.account().id(), &[note.id()], &[])
+        .build()
+        .execute();
+
+    match executed_transaction_other.unwrap_err() {
+        TransactionExecutorError::TransactionProgramExecutionFailed(
+            ExecutionError::FailedAssertion {
+                label: _,
+                source_file: _,
+                clk: _,
+                err_code: _,
+                err_msg,
+            },
+        ) => {
+            if let Some(ref msg) = err_msg {
+                assert!(msg.contains("the origin of the fungible asset is not this faucet"));
+            }
+        },
+        e => panic!("Expected TransactionProgramExecutionFailed error, got: {:?}", e),
+    }
+
+    // Execute the transaction against the receiver(BasicWallet).
+    // Should fail, but with a different error.
+    // `burn` procedure is not in the set of the receipient's account procedures.
+    let executed_transaction_receiver =
+        mock_chain.build_tx_context(receiver.id(), &[note.id()], &[]).build().execute();
+
+    match executed_transaction_receiver.unwrap_err() {
+        TransactionExecutorError::TransactionProgramExecutionFailed(
+            ExecutionError::EventError { label: _, source_file: _, error },
+        ) => {
+            if let Some(kernel_error) = error.downcast_ref::<TransactionKernelError>() {
+                match kernel_error {
+                    TransactionKernelError::UnknownAccountProcedure(_digest) => {},
+                    e => panic!("Expected UnknownAccountProcedure, got: {:?}", e),
+                }
+            } else {
+                panic!("Failed to downcast EventError to TransactionKernelError, got: {:?}", error);
+            }
+        },
+        e => panic!("Expected TransactionProgramExecutionFailed error, got: {:?}", e),
+    }
+
+    // Finally, execute the transaction against the original faucet. Should succeed
+    let executed_transaction = mock_chain
+        .build_tx_context(faucet.account().id(), &[note.id()], &[])
+        .build()
+        .execute();
+
+    assert!(executed_transaction.is_ok());
+    let executed_transaction = executed_transaction.unwrap();
+
+    faucet_account.apply_delta(executed_transaction.account_delta()).unwrap();
+    assert_eq!(faucet_account.storage().get_item(0).unwrap()[3], amount);
 }
