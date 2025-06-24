7023065743e940f23d3c961681d0013b2408fb2d
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-06-24 15:29:09 +0200

    feat: Make `LinkMapKey` generic over any word-convertible type

--- crates/miden-tx/src/host/link_map.rs
+++ crates/miden-tx/src/host/link_map.rs
@@ -300,9 +300,28 @@ pub struct EntryMetadata {
 
 /// The key in a [`LinkMap`].
 ///
-/// This is a wrapper around a [`Word`] implementing the link map ordering.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct LinkMapKey(Word);
+/// This is a wrapper around a type that can be converted to [`Word`] and overrides the equality
+/// and ordering implementations by implementing the link map ordering on the wrapped type's
+/// [`Word`] representation.
+#[derive(Debug, Clone, Copy)]
+pub struct LinkMapKey<T: Into<Word> = Word>(T);
+
+impl<T: Into<Word>> LinkMapKey<T> {
+    /// Wraps the provided value into a new [`LinkMapKey`].
+    pub fn new(inner: T) -> Self {
+        Self(inner)
+    }
+
+    /// Returns a reference to the inner value.
+    pub fn inner(&self) -> &T {
+        &self.0
+    }
+
+    /// Consumes self and returns the inner value.
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
 
 impl From<Word> for LinkMapKey {
     fn from(word: Word) -> Self {
@@ -310,25 +329,38 @@ impl From<Word> for LinkMapKey {
     }
 }
 
-impl From<LinkMapKey> for Word {
-    fn from(key: LinkMapKey) -> Self {
-        key.0
+impl<T: Into<Word>> From<LinkMapKey<T>> for Word {
+    fn from(key: LinkMapKey<T>) -> Self {
+        key.0.into()
     }
 }
 
-impl PartialOrd for LinkMapKey {
+impl<T: Into<Word> + Copy> PartialEq for LinkMapKey<T> {
+    fn eq(&self, other: &Self) -> bool {
+        let self_word: Word = self.0.into();
+        let other_word: Word = other.0.into();
+        self_word == other_word
+    }
+}
+
+impl<T: Into<Word> + Copy> Eq for LinkMapKey<T> {}
+
+impl<T: Into<Word> + Copy> PartialOrd for LinkMapKey<T> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for LinkMapKey {
+impl<T: Into<Word> + Copy> Ord for LinkMapKey<T> {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.0
+        let self_word: Word = self.0.into();
+        let other_word: Word = other.0.into();
+
+        self_word
             .iter()
             .rev()
             .map(Felt::as_int)
-            .zip(other.0.iter().rev().map(Felt::as_int))
+            .zip(other_word.iter().rev().map(Felt::as_int))
             .fold(Ordering::Equal, |ord, (felt0, felt1)| match ord {
                 Ordering::Equal => felt0.cmp(&felt1),
                 _ => ord,
