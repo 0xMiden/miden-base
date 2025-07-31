8326ecc58dd2eed9e1c020aae3f28d70c562ebc6
Bernhard Schuster <bernhard@ahoi.io>
2025-07-31 21:47:54 +0200

    delete trait TransactionProver, avoid `#[maybe_async]`

--- crates/miden-tx/src/prover/mod.rs
+++ crates/miden-tx/src/prover/mod.rs
@@ -20,26 +20,6 @@ pub use prover_host::TransactionProverHost;
 mod mast_store;
 pub use mast_store::TransactionMastStore;
 
-// TRANSACTION PROVER TRAIT
-// ================================================================================================
-
-/// The [TransactionProver] trait defines the interface that transaction witness objects use to
-/// prove transactions and generate a [ProvenTransaction].
-#[maybe_async_trait]
-pub trait TransactionProver {
-    /// Proves the provided transaction and returns a [ProvenTransaction].
-    ///
-    /// # Errors
-    /// - If the input note data in the transaction witness is corrupt.
-    /// - If the transaction program cannot be proven.
-    /// - If the transaction result is corrupt.
-    #[maybe_async]
-    fn prove(
-        &self,
-        tx_witness: TransactionWitness,
-    ) -> Result<ProvenTransaction, TransactionProverError>;
-}
-
 // LOCAL TRANSACTION PROVER
 // ------------------------------------------------------------------------------------------------
 
@@ -70,13 +50,11 @@ impl Default for LocalTransactionProver {
     }
 }
 
-#[maybe_async_trait]
-impl TransactionProver for LocalTransactionProver {
-    #[maybe_async]
+impl LocalTransactionProver {
     fn prove(
         &self,
         tx_witness: TransactionWitness,
-    ) -> Result<ProvenTransaction, TransactionProverError> {
+    ) -> impl Future<Output = Result<ProvenTransaction, TransactionProverError>> + Send {
         let TransactionWitness { tx_inputs, tx_args, advice_witness } = tx_witness;
 
         let account = tx_inputs.account();
@@ -116,57 +94,66 @@ impl TransactionProver for LocalTransactionProver {
 
         let advice_inputs = advice_inputs.into_advice_inputs();
 
-        let (stack_outputs, proof) = maybe_await!(prove(
-            &TransactionKernel::main(),
-            stack_inputs,
-            advice_inputs.clone(),
-            &mut host,
-            self.proof_options.clone(),
-        ))
-        .map_err(TransactionProverError::TransactionProgramExecutionFailed)?;
+        let proof_options = self.proof_options.clone();
+        async move {
+            // FIXME TODO this `fn prove()` is also `#[maybe_async]`
+            let (stack_outputs, proof) = prove(
+                &TransactionKernel::main(),
+                stack_inputs,
+                advice_inputs.clone(),
+                &mut host,
+                proof_options,
+            )
+            .await
+            .map_err(TransactionProverError::TransactionProgramExecutionFailed)?;
 
-        // extract transaction outputs and process transaction data
-        let (account_delta, output_notes, _tx_progress) = host.into_parts();
-        let tx_outputs =
-            TransactionKernel::from_transaction_parts(&stack_outputs, &advice_inputs, output_notes)
-                .map_err(TransactionProverError::TransactionOutputConstructionFailed)?;
+            // extract transaction outputs and process transaction data
+            let (account_delta, output_notes, _tx_progress) = host.into_parts();
+            let tx_outputs = TransactionKernel::from_transaction_parts(
+                &stack_outputs,
+                &advice_inputs,
+                output_notes,
+            )
+            .map_err(TransactionProverError::TransactionOutputConstructionFailed)?;
 
-        // erase private note information (convert private full notes to just headers)
-        let output_notes: Vec<_> = tx_outputs.output_notes.iter().map(OutputNote::shrink).collect();
-        let account_delta_commitment = account_delta.to_commitment();
+            // erase private note information (convert private full notes to just headers)
+            let output_notes: Vec<_> =
+                tx_outputs.output_notes.iter().map(OutputNote::shrink).collect();
+            let account_delta_commitment = account_delta.to_commitment();
 
-        let builder = ProvenTransactionBuilder::new(
-            account.id(),
-            account.init_commitment(),
-            tx_outputs.account.commitment(),
-            account_delta_commitment,
-            ref_block_num,
-            ref_block_commitment,
-            tx_outputs.expiration_block_num,
-            proof,
-        )
-        .add_input_notes(input_notes)
-        .add_output_notes(output_notes);
+            let builder = ProvenTransactionBuilder::new(
+                account.id(),
+                account.init_commitment(),
+                tx_outputs.account.commitment(),
+                account_delta_commitment,
+                ref_block_num,
+                ref_block_commitment,
+                tx_outputs.expiration_block_num,
+                proof,
+            )
+            .add_input_notes(input_notes)
+            .add_output_notes(output_notes);
 
-        // If the account is on-chain, add the update details.
-        let builder = match account.is_onchain() {
-            true => {
-                let account_update_details = if account.is_new() {
-                    let mut account = account.clone();
-                    account
-                        .apply_delta(&account_delta)
-                        .map_err(TransactionProverError::AccountDeltaApplyFailed)?;
+            // If the account is on-chain, add the update details.
+            let builder = match account.is_onchain() {
+                true => {
+                    let account_update_details = if account.is_new() {
+                        let mut account = account.clone();
+                        account
+                            .apply_delta(&account_delta)
+                            .map_err(TransactionProverError::AccountDeltaApplyFailed)?;
 
-                    AccountUpdateDetails::New(account)
-                } else {
-                    AccountUpdateDetails::Delta(account_delta)
-                };
+                        AccountUpdateDetails::New(account)
+                    } else {
+                        AccountUpdateDetails::Delta(account_delta)
+                    };
 
-                builder.account_update_details(account_update_details)
-            },
-            false => builder,
-        };
+                    builder.account_update_details(account_update_details)
+                },
+                false => builder,
+            };
 
-        builder.build().map_err(TransactionProverError::ProvenTransactionBuildFailed)
+            builder.build().map_err(TransactionProverError::ProvenTransactionBuildFailed)
+        }
     }
 }
