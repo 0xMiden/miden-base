ef6786d0ed04aab8d969844e4cbcf4901de96815
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-09-04 09:55:13 +0200

    chore: revert asset vault tests to next state

--- crates/miden-testing/src/kernel_tests/tx/test_asset_vault.rs
+++ crates/miden-testing/src/kernel_tests/tx/test_asset_vault.rs
@@ -158,53 +158,48 @@ fn test_has_non_fungible_asset() -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Tests that adding two different assets to the account vault succeeds.
 #[test]
 fn test_add_fungible_asset_success() -> anyhow::Result<()> {
-    let faucet_id1: AccountId = ACCOUNT_ID_PUBLIC_FUNGIBLE_FAUCET.try_into().unwrap();
-    let faucet_id2: AccountId = ACCOUNT_ID_PUBLIC_FUNGIBLE_FAUCET_2.try_into().unwrap();
-
-    let fungible_asset1 =
-        FungibleAsset::new(faucet_id1, FungibleAsset::MAX_AMOUNT - FUNGIBLE_ASSET_AMOUNT)?;
-    let fungible_asset2 = FungibleAsset::new(faucet_id2, FUNGIBLE_ASSET_AMOUNT)?;
-
-    let asset_note = NoteBuilder::new(faucet_id1, rand::rng())
-        .add_assets([fungible_asset1, fungible_asset2].map(Asset::from))
-        .build()?;
+    let tx_context = TransactionContextBuilder::with_existing_mock_account().build()?;
+    let mut account_vault = tx_context.account().vault().clone();
+    let faucet_id: AccountId = ACCOUNT_ID_PUBLIC_FUNGIBLE_FAUCET.try_into().unwrap();
+    let amount = FungibleAsset::MAX_AMOUNT - FUNGIBLE_ASSET_AMOUNT;
+    let add_fungible_asset = Asset::try_from(Word::new([
+        Felt::new(amount),
+        ZERO,
+        faucet_id.suffix(),
+        faucet_id.prefix().as_felt(),
+    ]))
+    .unwrap();
 
     let code = format!(
         "
-      use.mock::account
+        use.$kernel::prologue
+        use.mock::account
 
-      begin
-          push.{FUNGIBLE_ASSET1}
-          call.account::add_asset dropw
+        begin
+            exec.prologue::prepare_transaction
+            push.{FUNGIBLE_ASSET}
+            call.account::add_asset
 
-          push.{FUNGIBLE_ASSET2}
-          call.account::add_asset dropw
-      end
-      ",
-        FUNGIBLE_ASSET1 = Word::from(fungible_asset1),
-        FUNGIBLE_ASSET2 = Word::from(fungible_asset2)
+            # truncate the stack
+            swapw dropw
+        end
+        ",
+        FUNGIBLE_ASSET = Word::from(add_fungible_asset)
     );
 
-    let builder = ScriptBuilder::with_mock_libraries()?;
-    let source_manager = builder.source_manager();
-    let tx_script = builder.compile_tx_script(code)?;
-    let tx_context = TransactionContextBuilder::with_existing_mock_account()
-        .tx_script(tx_script)
-        .extend_input_notes(vec![asset_note])
-        .enable_partial_loading()
-        .with_source_manager(source_manager)
-        .build()?;
-    let account = tx_context.account().clone();
-    let tx = tx_context.execute_blocking()?;
+    let process = &tx_context.execute_code(&code)?;
 
-    let mut account_vault = account.vault().clone();
-    account_vault.add_asset(fungible_asset1.into())?;
-    account_vault.add_asset(fungible_asset2.into())?;
+    assert_eq!(
+        process.stack.get_word(0),
+        Into::<Word>::into(account_vault.add_asset(add_fungible_asset).unwrap())
+    );
 
-    assert_eq!(tx.final_account().vault_root(), account_vault.root());
+    assert_eq!(
+        process.get_kernel_mem_word(memory::NATIVE_ACCT_VAULT_ROOT_PTR),
+        account_vault.root()
+    );
 
     Ok(())
 }
