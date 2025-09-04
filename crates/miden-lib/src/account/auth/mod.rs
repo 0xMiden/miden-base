5d09496b7c9401646c31f23a728d44d11b3a93f8
Marti <marti@miden.team>
2025-09-04 08:08:17 +0000

    chore: rename module to rpo_falcon_512_multisig

--- crates/miden-lib/src/account/auth/mod.rs
+++ crates/miden-lib/src/account/auth/mod.rs
@@ -7,5 +7,5 @@ pub use rpo_falcon_512::AuthRpoFalcon512;
 mod rpo_falcon_512_acl;
 pub use rpo_falcon_512_acl::{AuthRpoFalcon512Acl, AuthRpoFalcon512AclConfig};
 
-mod multisig;
-pub use multisig::AuthRpoFalcon512Multisig;
+mod rpo_falcon_512_multisig;
+pub use rpo_falcon_512_multisig::AuthRpoFalcon512Multisig;
