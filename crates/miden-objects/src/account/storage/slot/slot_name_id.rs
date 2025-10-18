80a9fcb54180a54582e62e8eec4ffb43b6419d19
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-10-18 13:35:11 +0700

    feat: Impl `Display` for `SlotNameId`

--- crates/miden-objects/src/account/storage/slot/slot_name_id.rs
+++ crates/miden-objects/src/account/storage/slot/slot_name_id.rs
@@ -1,4 +1,6 @@
+use alloc::string::String;
 use core::cmp::Ordering;
+use core::fmt::Display;
 
 use crate::Felt;
 
@@ -21,6 +23,13 @@ impl SlotNameId {
     pub fn suffix(&self) -> Felt {
         self.suffix
     }
+
+    fn as_u128(&self) -> u128 {
+        let mut le_bytes = [0_u8; 16];
+        le_bytes[..8].copy_from_slice(&self.suffix().as_int().to_le_bytes());
+        le_bytes[8..].copy_from_slice(&self.prefix().as_int().to_le_bytes());
+        u128::from_le_bytes(le_bytes)
+    }
 }
 
 impl Ord for SlotNameId {
@@ -35,5 +44,28 @@ impl Ord for SlotNameId {
 impl PartialOrd for SlotNameId {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
+    }
+}
+
+impl Display for SlotNameId {
+    /// Returns a big-endian, hex-encoded string of length 34, including the `0x` prefix.
+    ///
+    /// This means it encodes 16 bytes.
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_fmt(format_args!("0x{:032x}", self.as_u128()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name_id_as_u128() {
+        let prefix = 1;
+        let suffix = 5;
+        let name_id = SlotNameId::new(Felt::from(prefix as u32), Felt::from(suffix as u32));
+        assert_eq!(name_id.as_u128(), (prefix << 64) + suffix);
+        assert_eq!(format!("{name_id}"), "0x00000000000000010000000000000005");
     }
 }
