f826fc5847b9687c75300a4fadf7e61622959d4a
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-10-28 13:37:46 +0800

    feat: Update `test_fee` to use names

--- crates/miden-testing/src/kernel_tests/tx/test_fee.rs
+++ crates/miden-testing/src/kernel_tests/tx/test_fee.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 use assert_matches::assert_matches;
-use miden_objects::account::{AccountId, StorageMap, StorageSlot};
+use miden_objects::account::{AccountId, NamedStorageSlot, StorageMap};
 use miden_objects::asset::{Asset, FungibleAsset, NonFungibleAsset};
 use miden_objects::note::NoteType;
 use miden_objects::testing::account_id::ACCOUNT_ID_NATIVE_ASSET_FAUCET;
@@ -132,8 +132,11 @@ async fn mutate_account_with_storage() -> anyhow::Result<ExecutedTransaction> {
     let account = builder.add_existing_mock_account_with_storage_and_assets(
         Auth::IncrNonce,
         [
-            StorageSlot::Value(rand_value()),
-            StorageSlot::Map(StorageMap::with_entries([(rand_value(), rand_value())])?),
+            NamedStorageSlot::randomly_named_value(rand_value()),
+            NamedStorageSlot::randomly_named_map(StorageMap::with_entries([(
+                rand_value(),
+                rand_value(),
+            )])?),
         ],
         [Asset::from(native_asset), NonFungibleAsset::mock(&[1, 2, 3, 4])],
     )?;
@@ -161,8 +164,11 @@ async fn create_output_notes() -> anyhow::Result<ExecutedTransaction> {
     let account = builder.add_existing_mock_account_with_storage_and_assets(
         Auth::IncrNonce,
         [
-            StorageSlot::Value(rand_value()),
-            StorageSlot::Map(StorageMap::with_entries([(rand_value(), rand_value())])?),
+            NamedStorageSlot::randomly_named_value(rand_value()),
+            NamedStorageSlot::randomly_named_map(StorageMap::with_entries([(
+                rand_value(),
+                rand_value(),
+            )])?),
         ],
         [Asset::from(native_asset), NonFungibleAsset::mock(&[1, 2, 3, 4])],
     )?;
