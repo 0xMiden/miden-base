b529aafe6db17a866ee5c1201430abc22f879fc2
Marti <marti@miden.team>
2025-11-04 09:07:00 +0000

    chore: expose "get_procedures" as public

--- crates/miden-objects/src/account/component/mod.rs
+++ crates/miden-objects/src/account/component/mod.rs
@@ -209,7 +209,7 @@ impl AccountComponent {
     }
 
     /// Returns a vector of tuples (digest, is_auth) for all procedures in this component.
-    pub(crate) fn get_procedures(&self) -> Vec<(Word, bool)> {
+    pub fn get_procedures(&self) -> Vec<(Word, bool)> {
         let mut procedures = Vec::new();
         for module in self.library.module_infos() {
             for (_, procedure_info) in module.procedures() {
