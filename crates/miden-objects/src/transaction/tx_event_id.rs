66bf6932995a4d5beda48f2675a60f760bde67e9
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-12-17 11:43:45 +0700

    chore: move tx event ID to objects

--- crates/miden-objects/src/transaction/tx_event_id.rs
+++ crates/miden-objects/src/transaction/tx_event_id.rs
@@ -2,7 +2,7 @@ use core::fmt;
 
 pub use miden_core::EventId;
 
-use super::TransactionEventError;
+use crate::errors::TransactionEventError;
 
 // CONSTANTS
 // ================================================================================================
