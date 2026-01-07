a21471b96a379e4eb5e5a2c8958986f958c5f054
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2026-01-07 14:49:36 +0100

    chore: Rename `create_random_note` to `*_default_*` and add pub helper

--- crates/miden-testing/src/kernel_tests/tx/test_lazy_loading.rs
+++ crates/miden-testing/src/kernel_tests/tx/test_lazy_loading.rs
@@ -96,7 +96,7 @@ async fn removing_fungible_assets_with_lazy_loading_succeeds() -> anyhow::Result
           # => []
 
           # move asset to note to adhere to asset preservation rules
-          exec.util::create_random_note_with_asset
+          exec.util::create_default_note_with_asset
           # => []
 
           push.{FUNGIBLE_ASSET2}
@@ -104,7 +104,7 @@ async fn removing_fungible_assets_with_lazy_loading_succeeds() -> anyhow::Result
           # => [ASSET]
 
           # move asset to note to adhere to asset preservation rules
-          exec.util::create_random_note_with_asset
+          exec.util::create_default_note_with_asset
           # => []
       end
       ",
